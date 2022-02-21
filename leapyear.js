c3ae5052c67bcf2e819fd55f4154f28bb255bba6
Amanda Suzzanne <amandasuzanne12@gmail.com>
2022-02-21 20:18:44 +0300

    leap year function

---
+++ leapyear.js
@@ -0,0 +1,9 @@
+//a function to determine whether an year is a lep year or not
+
+function leapyear(year){
+    if(year%4==0){
+        console.log('${year} is a leap year');
+    }else{
+        console.log('${year} is not a leap year');
+    }
+}
