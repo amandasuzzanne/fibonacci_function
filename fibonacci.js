cd32910ae8c15fafca51a3d7f67b77ac21c0e1ea
Brown <45181428+Godwin254@users.noreply.github.com>
2023-02-24 17:32:47 +0300

    Update fibonacci implementaion
    - Add alternative and optimized option 
    - Add test cases

--- fibonacci.js
+++ fibonacci.js
@@ -1,13 +1,43 @@
-function fib(n){
-let a = 0;
-let b = 1;
-if(n==a || n==b) return true;
-let c = a+b;
-while(c<=n){
-if(c==n) return true;
-a=b;
-b=c;
-c=a+b;
+const fibonacci = (n) => {
+  //base case
+  if ( n < 2) return n;
+    
+  // f(n) = f(n-1) + f(n-2)
+  return fibonacci(n-1) + fibonacci(n-2);
 }
-return false;
+
+//prev implemention may have some complexities given a case input is extremely large
+//so alternatively
+
+const fibonacci2 = (n) => {
+  const sequence = [0,1];
+  
+  for (let i = 2; i < sequence.length; i++){
+    sequence.push(sequence[i-1] + sequence[i-2]);
+  }
+  return sequence[n];
 }
+
+
+//test -remove the .skip to run the implentation against the test
+test.skip("Test Fibonacci function1", () => {
+  const num = 1;
+  const num2 = 5;
+  const answer = fibonacci(num);
+  const answer2 = fibonacci(num2)
+  
+  expect(answer).toBe(1)
+  expect(answer2).toBe(5)
+})
+
+
+//test -remove the .skip to run the implentation against the test
+test.skip("Test Fibonacci function2", () => {
+  const num = 6;
+  const num2 = 100;
+  const answer = fibonacci(num);
+  const answer2 = fibonacci(num2)
+  
+  expect(answer).toBe(1)
+  expect(answer2).toBe(5)
+})
